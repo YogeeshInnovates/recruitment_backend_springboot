a1b75b6d80e8fbac46e773c4f61df19cdb926c5f
Yogeesh Hegde <yogeeshhegde778@gmail.com>
2026-08-03 23:56:05 +0530

    Fix LazyInitializationException in reminder scheduler (add @Transactional); fix ghcr make-public step

--- src/main/java/com/recruitment/scheduler/InterviewScheduler.java
+++ src/main/java/com/recruitment/scheduler/InterviewScheduler.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -26,6 +27,7 @@ public class InterviewScheduler {
     private String frontendUrl;
 
     @Scheduled(fixedRate = 60000)
+    @Transactional
     public void processScheduledInterviews() {
         log.debug("Running interview scheduler check...");
 
